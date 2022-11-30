1579ad2508dad92e7104991847c67d980873312f
GayathriArdhalapudi <gayathriardhalapudi@gmail.com>
2022-11-30 16:05:24 +0530

    Hall project first commit

---
+++ Hall.java
@@ -0,0 +1,9 @@
+1)git init
+2)git add README.md
+3)git commit -m "Halo project first commit"
+4)git branch -M main
+5)git remote add origin https://github.com/GayathriArdhalapudi/Halo.git
+6)git push -u origin main
+git config--global--
+ git config --global user.email "gayathriardhalapudi@gmail.com"
+ git config --global user.name "GayathriArdhalapudi"
